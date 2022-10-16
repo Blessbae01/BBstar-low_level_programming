c26e97eb38bb460d8d763889ccb5c4498a1e02d4
Blessbae01 <uzoukwublessing34@gmail.com>
2022-10-16 09:33:39 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
+
 /**
- * main-program entry point.
- * Return: 0 if no error, non zero if error.
+ * main - prints all the numbers of base 16 in lowercase,
+ * followed by a new line.
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int i;
-	char hexvalues[] = "0123456789abcdef";
+	int n;
+	char ch;
 
-	for (i = 0; i < 16; i++)
+	for (n = 48; n < 58; n++)
 	{
-		putchar(hexvalues[i]);
+		putchar(n);
+	}
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
 	}
 	putchar('\n');
 	return (0);
